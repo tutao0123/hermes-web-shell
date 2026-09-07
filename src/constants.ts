04427280acde7f50438c9baedb7753c28eb621ff
wentao tao <wentaotao666@gmail.com>
2026-09-07 19:36:59 +0800

    Add i18n, theme prefs, and Vite config

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_PASSWORD = 'demo'
+export const AUTH_STORAGE_KEY = 'hermes-web-shell-auth'
+export const THEME_STORAGE_KEY = 'hermes-web-shell-theme'
+export const LOCALE_STORAGE_KEY = 'hermes-web-shell-locale'
